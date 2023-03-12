d8d077b59eba0b21211e32ba0fef224d7bc34c7c
Kai <human2lkai@gmail.com>
2023-03-12 11:44:26 +1100

    Exercise 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import Header from '../Header';
-import ShoeIndex from '../ShoeIndex';
+import Header from "../Header";
+import ShoeIndex from "../ShoeIndex";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+  const [sortId, setSortId] = React.useState("newest");
 
   return (
-    <>
+    <Wrapper>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
+    </Wrapper>
   );
 };
 
